f6854405610f9b724f1e0af7de820208e7eaa8b8
Pedro Ramos <ppedro.ramoss@gmail.com>
2015-08-23 18:48:00 -0300

    Ajusta logica da busca na arvore de cliques, corrige alguns erros de compilação

--- include/PercolationVerifier.h
+++ include/PercolationVerifier.h
@@ -1,6 +1,10 @@
 #ifndef PERCOLATIONVERIFIER_H
 #define PERCOLATIONVERIFIER_H
 
+#include <set>
+#include <list>
+
+using namespace std;
 
 class PercolationVerifier
 {
@@ -8,44 +12,7 @@ class PercolationVerifier
         PercolationVerifier();
         virtual ~PercolationVerifier();
 
-        unsigned getPercolatingClique(*set<unsigned> cliqueNodes) {
-            return recursivePercolationVerifier(cliqueNodes, cliqueTree->root;)
-        }
-
-    protected:
-    private:
-        struct TNode {
-            unsigned idClique;
-            set<unsigned> graphNodes;
-            TNode *left;
-            TNode *right;
-        };
-
-        struct TTree {
-            TNode *root;
-        };
-
-        TTree cliqueTree;
-
-        unsigned recursivePercolationVerifier(*set<unsigned> cliqueNodes, TNode *currentTreeNode) {
-
-            if (currentTreeNode->left == NULL and currentTreeNode->right == NULL) {
-                return currentTreeNode->idClique;
-            } else {
-
-                if (doTheseCliquesPercolate(cliqueNodes, currentTreeNode->left->graphNodes)) {
-                    currentTreeNode = currentTreeNode->left;
-                } else if (doTheseCliquesPercolate(cliqueNodes, currentTreeNode->right->graphNodes)) {
-                    currentTreeNode = currentTreeNode->right;
-                } else {
-                    return -1;
-                }
-
-                return recursivePercolationVerifier(cliqueNodes, currentTreeNode);
-            }
-        }
-
-        bool doTheseCliquesPercolate(*set<unsigned> clique1, *set<unsigned> clique2, unsigned numberOfNodesRequired) {
+        bool doTheseCliquesPercolate(set<unsigned> *clique1, set<unsigned> *clique2, unsigned numberOfNodesRequired) {
 
             bool percolate;
 
@@ -57,6 +24,69 @@ class PercolationVerifier
 
             return percolate;
         }
+
+
+        list<unsigned> getPercolatingCliques(set<unsigned> *cliqueNodes) {
+            recursivePercolationVerifier(cliqueNodes, cliqueTree.root);
+            return lastResult;
+        }
+
+        struct TNode {
+            unsigned idClique;
+            set<unsigned> graphNodes;
+            TNode *left;
+            TNode *right;
+            bool visited;
+            bool leaf;
+        };
+
+        struct TTree {
+            TNode *root;
+        };
+
+        TTree cliqueTree;
+
+        list<unsigned> lastResult;
+
+        bool recursivePercolationVerifier(set<unsigned> *cliqueNodes, TNode *currentTreeNode) {
+
+            if (currentTreeNode->left != NULL) {
+                if (!currentTreeNode->left->visited) {
+                    if (doTheseCliquesPercolate(*cliqueNodes, currentTreeNode->left->graphNodes, 1) {
+                        // if it's a leaf node, then it's a percolating clique and needs to be added to the lastResult list
+                        if (currentTreeNode->left.leaf) {
+                            lastResult.add(currentTreeNode->left.idClique);
+                        } else {
+                        // if it's not a leaf, the recursion tree keeps going until it has found one
+                            recursivePercolationVerifier(cliqueNodes, currentTreeNode->left);
+                        }
+                    }
+                }
+            }
+
+            if (currentTreeNode->right != NULL) {
+                if (!currentTreeNode->right.visited) {
+                    if (doTheseCliquesPercolate(cliqueNodes, currentTreeNode->right->graphNodes)) {
+                        // if it's a leaf node, then it's a percolating clique and needs to be added to the lastResult list
+                        if (currentTreeNode->right.leaf) {
+                            lastResult.add(currentTreeNode->right.idClique);
+                        } else {
+                        // if it's not a leaf, the recursion tree keeps going until it has found one
+                            recursivePercolationVerifier(cliqueNodes, currentTreeNode->right);
+                        }
+                    }
+                }
+            }
+
+            currentTreeNode.visited = true;
+
+            return true;
+        }
+
+
+    protected:
+    private:
+
 };
 
 #endif // PERCOLATIONVERIFIER_H
--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 
 using namespace std;
 
+
 int main()
 {
+
     return 0;
 }
