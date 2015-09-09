ec0075e9bccece452f3f4ed9c078f119a38cb76c
Pedro Ramos <ppedro.ramoss@gmail.com>
2015-09-08 23:18:44 -0300

    Continuando insersão

--- include/PercolationVerifier.h
+++ include/PercolationVerifier.h
@@ -9,6 +9,16 @@
 using namespace std;
 
 
+
+set<unsigned> setUnion(set<unsigned> &set1, set<unsigned> &set2) {
+
+    set<unsigned> unionSet;
+    set_union(set1.begin(), set1.end(), set2.begin(), set2.end(), inserter(unionSet, unionSet.end()));
+
+    return unionSet;
+
+}
+
 bool doTheseCliquesPercolate(set<unsigned> &clique1, set<unsigned> &clique2, unsigned numberOfNodesRequired) {
 
     bool percolate;
@@ -24,9 +34,12 @@ bool doTheseCliquesPercolate(set<unsigned> &clique1, set<unsigned> &clique2, uns
 
 struct TNode {
     unsigned idClique;
-    set<unsigned> graphNodes;
+    set<unsigned> *graphNodes;
     TNode *left;
     TNode *right;
+    TNode *leftBrother;
+    TNode *rightBrother;
+    TNode *parent;
     bool visited;
     bool leaf;
 };
@@ -40,7 +53,7 @@ list<unsigned*> lastResult;
 
 TNode* lastResultParentWithSingleChild;
 
-bool recursivePercolationVerifier(set<unsigned> &cliqueNodes, TNode &currentTreeNode) {
+/*bool recursivePercolationVerifier(set<unsigned> &cliqueNodes, TNode &currentTreeNode) {
 
     if (currentTreeNode.left != NULL) {
         if (!currentTreeNode.left->visited) {
@@ -79,9 +92,167 @@ list<unsigned*> getPercolatingCliques(set<unsigned> *cliqueNodes, TTree *cliqueT
     //lastResult = -1;
     recursivePercolationVerifier(*cliqueNodes, *cliqueTree->root);
     return lastResult;
+}*/
+
+TNode *newTNode() {
+
+    TNode *tNode = new TNode;
+
+    // memoria gasta a toa, revisar
+    // quando é parent node não aproveita esse set criado aqui
+    tNode->graphNodes = new set<unsigned>();
+
+    tNode->idClique = -1;
+    tNode->leaf = false;
+    tNode->left = NULL;
+    tNode->right = NULL;
+    tNode->leftBrother = NULL;
+    tNode->rightBrother = NULL;
+    tNode->parent = NULL;
+    tNode->visited = false;
+
+    return tNode;
+
 }
 
-// Looks for an empty space to add a child on
+TNode *lastParentCreated;
+
+void buildCliqueTree(list<set<unsigned>> *cliquesList) {
+
+    TNode *nav;
+    lastParentCreated = NULL;
+
+    // Cliques/leafs
+    unsigned listSize = cliquesList->size();
+    for (unsigned i = 0; i < listSize; i+=2) {
+
+
+        TNode *newCliqueLeft = newTNode();
+
+        newCliqueLeft->idClique = i;
+        newCliqueLeft->leaf = true;
+        newCliqueLeft->graphNodes = &cliquesList->front();
+
+        cliquesList->pop_front();
+
+        if (!cliquesList->empty()) {
+
+            TNode *newCliqueRight = newTNode();
+            newCliqueRight->idClique = i+1;
+            newCliqueRight->leaf = true;
+
+            newCliqueRight->graphNodes = &cliquesList->front();
+            cliquesList->pop_front();
+
+            newCliqueLeft->rightBrother = newCliqueRight;
+            newCliqueRight->leftBrother = newCliqueLeft;
+
+            TNode *newCliqueParent = newTNode();
+            newCliqueParent->left = newCliqueLeft;
+            newCliqueParent->right = newCliqueRight;
+
+            set<unsigned> unionSet;
+            set_union(newCliqueLeft->graphNodes->begin(), newCliqueLeft->graphNodes->end(), newCliqueRight->graphNodes->begin(), newCliqueRight->graphNodes->end(), inserter(unionSet, unionSet.end()));
+            //set<unsigned> parentNodes = setUnion(*newCliqueLeft->graphNodes, *newCliqueRight->graphNodes);
+            newCliqueParent->graphNodes = &unionSet;
+
+            /*if (lastParentCreated != NULL) {
+                lastParentCreated->rightBrother = newCliqueParent;
+                newCliqueParent->leftBrother = lastParentCreated;
+            }*/
+
+            lastParentCreated = newCliqueParent;
+
+        }
+
+        if (i == 0) {
+            nav = newCliqueLeft;
+
+        }
+
+
+    }
+
+    //cout << lastParentCreated->graphNodes->size();
+
+    /*while (nav != NULL ) {
+
+        cout << nav->idClique << endl;
+
+        if (nav != NULL) {
+            nav = nav->rightBrother;
+        }
+
+
+    }*/
+
+
+}
+
+/*void buildCliqueTree(list<set<unsigned>> *cliquesList) {
+
+    TNode *nav;
+
+    // Cliques/leafs
+    unsigned listSize = cliquesList->size();
+    for (unsigned i = 0; i < listSize; i+=2) {
+
+
+        TNode *newCliqueLeft = new TNode();
+
+        newCliqueLeft->idClique = i;
+        newCliqueLeft->leaf = true;
+        newCliqueLeft->graphNodes = cliquesList->front();
+
+        cliquesList->pop_front();
+
+        if (!cliquesList->empty()) {
+
+            TNode *newCliqueRight = new TNode();
+            newCliqueRight->idClique = i+1;
+            newCliqueRight->leaf = true;
+
+            newCliqueRight->graphNodes = cliquesList->front();
+            cliquesList->pop_front();
+
+            newCliqueLeft->rightBrother = newCliqueRight;
+            newCliqueRight->leftBrother = newCliqueLeft;
+        }
+
+        if (i == 0) {
+            nav = newCliqueLeft;
+
+        }
+
+
+        TNode newCliqueParent;
+        newCliqueParent.left = newCliqueLeft;
+        newCliqueParent.right = newCliqueRight;
+
+        newCliqueParent.graphNodes = setunion(newCliqueLeft, newCliqueRight);
+
+    }
+
+
+
+    while (nav->rightBrother != NULL ) {
+
+        cout << nav->idClique << endl;
+
+        if (nav->rightBrother != NULL) {
+            nav = nav->rightBrother;
+        }
+
+
+    }
+
+
+}*/
+
+
+
+
+/*// Looks for an empty space to add a child on
 // Basically it looks for a parent node with just one child node
 // Returns the parent node itself
 void recursivellyFindParentWithSigleChild(TNode &currentTreeNode) {
@@ -113,6 +284,6 @@ TNode* findParentWithSingleChild() {
     lastResultParentWithSingleChild = NULL;
     recursivellyFindParentWithSigleChild(TTree.root);
     return lastResultParentWithSingleChild;
-}
+}*/
 
 #endif // PERCOLATIONVERIFIER_H
--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@
 
 using namespace std;
 
-
-int main()
-{
+/*void test1() {
     set<unsigned> nodes;
     nodes.insert(1);
     nodes.insert(2);
@@ -47,6 +45,90 @@ int main()
     list<unsigned*> result = getPercolatingCliques(&clique1, &cliqueTree);
     cout << *result.front();
     cout << *result.back();
+}*/
 
+void test2() {
+
+    set<unsigned> clique1;
+    clique1.insert(1);
+    clique1.insert(2);
+    clique1.insert(3);
+
+    set<unsigned> clique2;
+    clique2.insert(2);
+    clique2.insert(3);
+    clique2.insert(4);
+
+    set<unsigned> clique3;
+    clique2.insert(2);
+    clique2.insert(3);
+    clique2.insert(4);
+
+    set<unsigned> clique4;
+    clique2.insert(2);
+    clique2.insert(3);
+    clique2.insert(4);
+
+
+    list<set<unsigned>> cliquesList;
+    cliquesList.push_back(clique1);
+    cliquesList.push_back(clique2);
+    cliquesList.push_back(clique3);
+    cliquesList.push_back(clique4);
+
+    buildCliqueTree(&cliquesList);
+
+}
+
+
+struct TElement {
+
+    TElement *next;
+    unsigned id;
+
+};
+
+struct TCollection {
+
+    TElement *first;
+
+};
+
+void learn() {
+
+    TCollection *col = new TCollection;
+
+    col->first = new TElement;
+    col->first->id = 0;
+
+    TElement *nav = col->first;
+
+    for (int i = 1; i < 10; i++) {
+
+        TElement *newElement = new TElement;
+        newElement->id = i;
+
+        nav->next = newElement;
+        nav = newElement;
+
+    }
+
+    nav = col->first;
+
+    for (int i = 0; i < 10; i++) {
+
+        cout << nav->id;
+        nav = nav->next;
+
+    }
+
+
+}
+
+
+int main()
+{
+
+    test2();
     return 0;
 }
